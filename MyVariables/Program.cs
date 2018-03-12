0527614d0e01c82e74110b197a5a9a6e40e87681
Administrator <Administrator@VMA16D4A7>
2018-03-12 19:28:13 +0100

    add comment

---
+++ MyVariables/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int _myValue = 7;
+            Console.Write("{0}", _myValue);
+        }
+    }
+}
